9eff5de7f5b7630b8ed551a571e3c4bb5fbe1d31
Tobias Årud <me@tobiasarud.se>
2016-05-15 20:55:25 +0200

    Day 1: Basics
    Setting up the basics of this edition of the theme.

---
+++ functions.php
@@ -0,0 +1,49 @@
+<?php
+
+if(!function_exists('eruma_yon_setup')):
+    function eruma_yon_setup(){
+        load_theme_textdomain('eruma-yon', get_template_directory() . '/languages');
+        add_theme_support('automatic-feed-links');
+        add_theme_support('title-tag');
+        add_theme_support('post-thumbnails');
+        
+        add_theme_support( 'custom-logo', array(
+            'height'      => 248,
+            'width'       => 248,
+            'flex-height' => true,
+        ));
+        
+        // Register Menus
+        register_nav_menus( array(
+            'primary' => esc_html__('Primary Menu', 'eruma-yon'),
+        ));
+        
+        // Change default core markup for the items to html5 code.
+        add_theme_support('html5', array(
+            'search-form',
+            'comment-form',
+            'comment-list',
+            'gallery',
+            'caption',
+        ));
+    }
+endif; // eruma_yon_setup
+add_action( 'after_setup_theme', 'eruma_yon_setup' );
+
+
+if(!function_exists('eruma_yon_the_custom_logo')):
+function eruma_yon_the_custom_logo(){
+	if (function_exists('the_custom_logo')){
+		the_custom_logo();
+	}
+}
+endif;
+
+function eruma_yon_scripts() {
+    wp_enqueue_style( 'custom-fonts', 'https://fonts.googleapis.com/css?family=Lato:400,700,400italic|Oswald:400,700,300|Vollkorn:400,400italic|Source+Code+Pro:300', false );
+    
+	wp_enqueue_style( 'eruma-yon-style', get_stylesheet_uri() );
+
+	wp_enqueue_script( 'font-awesome', 'https://use.fontawesome.com/baf6baca4e.js', false );
+}
+add_action( 'wp_enqueue_scripts', 'eruma_yon_scripts' );
---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="profile" href="http://gmpg.org/xfn/11">
+    <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
+    <?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+    <div id="page" class="hfeed site">
+        <header id="masthead" class="site-header" role="banner">
+            <div class="site-branding">
+                <?php
+                    eruma_yon_the_custom_logo();
+
+                    if (is_front_page() && is_home()): ?>
+                        <h1 class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>" rel="home"><?php bloginfo('name'); ?></a></h1>
+                    <?php else : ?>
+                        <p class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>" rel="home"><?php bloginfo('name'); ?></a></p>
+                    <?php endif;
+
+                    $description = get_bloginfo( 'description', 'display' );
+                    if ($description || is_customize_preview()): ?>
+                        <p class="site-description"><?php echo $description; ?></p>
+                    <?php endif;
+                ?>
+            </div><!-- .site-branding -->
+            <nav id="site-navigation" class="main-navigation" role="navigation">
+                <?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
+            </nav><!-- #site-navigation -->
+        </header><!-- #masthead .site-header -->
+        <footer></footer>
+    </div><!-- #page .hfeed .site -->
+    <?php wp_footer(); ?>
+</body>
+</html>
